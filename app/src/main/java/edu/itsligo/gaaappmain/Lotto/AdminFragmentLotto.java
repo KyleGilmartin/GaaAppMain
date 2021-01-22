6c4a357ece04d24bd25da6b4e9697c64f2e36b30
kyle-gilmartin <kylegilmartin8@gmail.com>
2021-01-22 01:37:59 +0000

    Lotto now working

--- app/src/main/java/edu/itsligo/gaaappmain/Lotto/AdminFragmentLotto.java
+++ app/src/main/java/edu/itsligo/gaaappmain/Lotto/AdminFragmentLotto.java
@@ -1,22 +1,25 @@
 package edu.itsligo.gaaappmain.Lotto;
 
+import android.app.Dialog;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import java.util.List;
 import java.util.Random;
-
 import edu.itsligo.gaaappmain.ModelUsers;
 import edu.itsligo.gaaappmain.R;
 
@@ -27,13 +30,17 @@ public class AdminFragmentLotto extends Fragment {
     private Button btn_winner;
     private TextView tv_winner;
     private FirebaseFirestore FireStore;
+    private DatabaseReference databaseReference;
+    private Dialog progressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_admin_lotto, container, false);
         btn_winner = view.findViewById(R.id.btWinner);
         tv_winner = view.findViewById(R.id.tvHomeLottoWinner);
-
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        dialogBuilder.setView(R.layout.progress);
+        progressDialog = dialogBuilder.create();
         return view;
     }
 
@@ -43,9 +50,27 @@ public class AdminFragmentLotto extends Fragment {
 
         FireStore = FirebaseFirestore.getInstance();
         btn_winner.setOnClickListener(v-> pickUser());
+        progressDialog.show();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("LottoWinner").child("UserName");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                tv_winner.setText(snapshot.getValue(String.class));
+                progressDialog.dismiss();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(),"Failed to connect", Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
+            }
+        });
     }
 
     private void pickUser(){
+
+        progressDialog.show();
         FireStore.collection("Users").get()
                 .addOnSuccessListener(queryDocumentSnapshots -> {
 
@@ -54,6 +79,15 @@ public class AdminFragmentLotto extends Fragment {
                         int random = new Random().nextInt(list.size());
 
                         ModelUsers user_model = list.get(random).toObject(ModelUsers.class);
+                        String winner_name = user_model.getFullName();
+
+                        databaseReference.setValue(winner_name).addOnCompleteListener(task -> {
+                            tv_winner.setText(winner_name);
+                            progressDialog.dismiss();
+                        }).addOnFailureListener(e -> {
+                            Toast.makeText(getContext(),"Failed to connect", Toast.LENGTH_LONG).show();
+                            progressDialog.dismiss();
+                        });
                         tv_winner.setText(user_model.getFullName());
 
                     }
--- app/src/main/java/edu/itsligo/gaaappmain/Lotto/HomeFragmentLotto.java
+++ app/src/main/java/edu/itsligo/gaaappmain/Lotto/HomeFragmentLotto.java
@@ -1,28 +1,61 @@
 package edu.itsligo.gaaappmain.Lotto;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import edu.itsligo.gaaappmain.R;
 
 
 public class HomeFragmentLotto extends Fragment {
 
-
+    private TextView tv_winner;
+    private Dialog progressDialog;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v=  inflater.inflate(R.layout.fragment_home_lotto, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_lotto, container, false);
+        tv_winner = view.findViewById(R.id.tvHomeLottoWinner2);
 
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        dialogBuilder.setView(R.layout.progress);
+        progressDialog = dialogBuilder.create();
+        return view;
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        return  v;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("LottoWinner");
+
+        progressDialog.show();
+        myRef.child("UserName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                tv_winner.setText(snapshot.getValue(String.class));
+                progressDialog.dismiss();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(),"Failed to connect", Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
+            }
+        });
     }
 }
